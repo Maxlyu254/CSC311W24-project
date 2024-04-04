d36ba964a708fa48b75b2e73c644aa59994705a0
MikeJiahaoXu <mikeke.xu@mail.utoronto.ca>
2024-04-04 19:04:47 -0400

    .

--- part_a/knn.py
+++ part_a/knn.py
@@ -1,5 +1,6 @@
 from sklearn.impute import KNNImputer
 from utils import *
+from matplotlib import pyplot as plt
 
 
 def knn_impute_by_user(matrix, valid_data, k):
@@ -37,7 +38,11 @@ def knn_impute_by_item(matrix, valid_data, k):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
-    acc = None
+    nbrs = KNNImputer(n_neighbors=k)
+    # We use NaN-Euclidean distance measure.
+    mat = nbrs.fit_transform(matrix.T).T
+    acc = sparse_matrix_evaluate(valid_data, mat)
+    print("Validation Accuracy: {}".format(acc))
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -60,7 +65,41 @@ def main():
     # the best performance and report the test accuracy with the        #
     # chosen k*.                                                        #
     #####################################################################
-    pass
+    accuracy_user = []
+    best_k_user = 0
+    for k in [1, 6, 11, 16, 21, 26]:
+        acc = knn_impute_by_user(sparse_matrix, val_data, k)
+        accuracy_user.append(acc)
+        if acc == max(accuracy_user):
+            best_k_user = k
+
+    plt.plot([1, 6, 11, 16, 21, 26], accuracy_user)
+    plt.xlabel('k')
+    plt.ylabel('Accuracy')
+    plt.title("Accuracy of kNN by user")
+    plt.savefig("knn_user.png")
+    plt.show()
+
+    print("Best k for user: {}".format(best_k_user))
+    print("Test accuracy with the best k for user: {}".format(knn_impute_by_user(sparse_matrix, test_data, best_k_user)))
+
+    accuracy_item = []
+    best_k_item = 0
+    for k in [1, 6, 11, 16, 21, 26]:
+        acc = knn_impute_by_item(sparse_matrix, val_data, k)
+        accuracy_item.append(acc)
+        if acc == max(accuracy_item):
+            best_k_item = k
+    
+    plt.plot([1, 6, 11, 16, 21, 26], accuracy_item)
+    plt.xlabel('k')
+    plt.ylabel('Accuracy')
+    plt.title("Accuracy of kNN by item")
+    plt.savefig("knn_item.png")
+    plt.show()
+
+    print("Best k for item: {}".format(best_k_item))
+    print("Test accuracy with the best k for item: {}".format(knn_impute_by_item(sparse_matrix, test_data, best_k_item)))
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
